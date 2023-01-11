5c28abc248c68777ba8db787cd8a4b2d3b7d66b9
lruizpandaid <leoarruizz@gmail.com>
2023-01-11 14:38:47 -0400

    prueba

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from flask import Flask, request, jsonify, make_response
 import uuid
 import json
 import time
+import argparse
 from tqdm import tqdm
 
 #------------------------------
@@ -116,3 +117,13 @@ def analyzeWrapper(req, trx_id = 0):
 	#---------------
 	#print(resp_obj)
 	return resp_obj
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser()
+	parser.add_argument(
+		'-p', '--port',
+		type=int,
+		default=5000,
+		help='Port of serving api')
+	args = parser.parse_args()
+	app.run(host='0.0.0.0', port=args.port)
